273e13a386b18e24e7bba73174ee9c12bb642774
Alexander Eide <atlexeide@gmail.com>
2024-10-23 12:35:44 +0200

    Add log function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { Tree } from "./classes.js";
-const arr = [1, 6345, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 324];
+const arr = [1, 6345, 7, 4, 23, 8, 9, 4, 3, 5, 67, 324];
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const test = new Tree(arr);
@@ -7,4 +7,13 @@ test.buildTree();
 console.log(test.array);
 console.log(test.root);
 console.log(test.root.left);
+console.log(test.root.right);
+
+function preOrder(root) {
+  if (root === null) return;
+  console.log(root.data);
+  preOrder(root.left);
+  preOrder(root.right);
+}
+preOrder(test.root);
 console.log(test.root.right);
