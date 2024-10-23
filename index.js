788cb60abe6e752c7f1e6c07d6b90cb9297d08c1
Alexander Eide <atlexeide@gmail.com>
2024-10-23 10:26:49 +0200

    Update logic

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import { Tree } from "./classes.js";
 import { Node } from "./classes.js";
 const test = new Tree();
 const node = new Node();
-const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+// const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 test.buildTree(testArr);
 
@@ -24,3 +25,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 prettyPrint(test.root);
+console.log(test.root.right);
