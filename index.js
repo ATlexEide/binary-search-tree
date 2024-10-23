5e6a98d72c528834cee52cdb8fefa799de0ce9bd
Alexander Eide <atlexeide@gmail.com>
2024-10-23 13:15:24 +0200

    Remove console logs

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const arr = [1, 6345, 7, 4, 23, 8, 9, 4, 3, 5, 67, 324];
 
 const test = new Tree(arr);
 test.buildTree();
-console.log(test.array);
-console.log(test.root);
-console.log(test.root.left);
-console.log(test.root.right);
 
 function preOrder(root) {
   if (root === null) return;
